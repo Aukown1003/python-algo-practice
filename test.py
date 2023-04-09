bf0e641df0f1d866b75bdb628b0d0b9fbe9a4cd1
Auk1003 <Aukown1003@users.noreply.github.com>
2023-04-09 18:48:47 +0900

    use math

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
+import math
+
 num = 1
-name: str = '1'
+name = '1'
 
 print(num)
 print(name)
 
 new_num = int(name)
 print(new_num, type(new_num))
+# sep= 単語の間に入れるワード　end=最後の入れる文字や処理 option + ¥でバックスラッシュ
+print('hi', 'mike', 'yu', sep=',', end='.\n')
+
+result = math.sqrt(25)
+print(result)
