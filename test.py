2f70eb1504639c4501c829ed4c231dd266dfe560
Auk1003 <Aukown1003@users.noreply.github.com>
2023-04-09 18:59:58 +0900

    use text

--- test.py
+++ test.py
@@ -13,3 +13,21 @@ print('hi', 'mike', 'yu', sep=',', end='.\n')
 
 result = math.sqrt(25)
 print(result)
+
+print('hello.\nhou wow you')
+# \nを文字列で使いたい場合は戦闘にrをおいておく
+print(r'c:\name\name')
+# 複数行書くときは""""""を入れる
+print("""
+line1
+line2
+line3
+""")
+
+# 繰り返し処理
+print('hi' * 3 + 'mike')
+
+# 長い文字列は改行で
+s = ('aaaaaaaaaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbbbbbbbbb')
+print(s)
